fc5745b24e237a50c6355cb3c12647594e92b740
AlexBor78 <you@example.com>
2026-06-29 15:03:41 +0300

    added docker

--- nix/hosts/anton/default.nix
+++ nix/hosts/anton/default.nix
@@ -10,6 +10,7 @@
 
   # docker dir
   systemd.tmpfiles.rules = [ "d /srv 0755 root root -" ];
+	virtualisation.docker.enable = true;
 
   users.users.${username}.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVmKG0A3mAahs3zXBW7BSt9Beb8yf6AAyeYvhNod8+L admin@lexa-projects.ru"
