0a4e1738eb060153462a90904f5e2eb8d16af9a3
AlexBor78 <you@example.com>
2026-06-29 14:01:34 +0300

    added ssh key

--- nix/hosts/anton/default.nix
+++ nix/hosts/anton/default.nix
@@ -1,6 +1,6 @@
 # /nix/hosts/t480/default.nix
 
-{ config, lib, pkgs, modulesPath, ...} : {
+{ config, lib, pkgs, username, ...} : {
 	imports = [
 		./hardware-configuration.nix
     ./network.nix
@@ -10,6 +10,10 @@
 
   # docker dir
   systemd.tmpfiles.rules = [ "d /srv 0755 root root -" ];
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVmKG0A3mAahs3zXBW7BSt9Beb8yf6AAyeYvhNod8+L admin@lexa-projects.ru"
+  ];
 
 
   # sops-nix
--- nix/hosts/maria/default.nix
+++ nix/hosts/maria/default.nix
@@ -8,6 +8,10 @@
     ./ssh.nix
 	];
 
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVmKG0A3mAahs3zXBW7BSt9Beb8yf6AAyeYvhNod8+L admin@lexa-projects.ru"
+  ];
+
   # docker dir
   systemd.tmpfiles.rules = [ "d /srv 0755 root root -" ];
 }
