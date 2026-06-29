026bf14adc52a38c43b63c863b66b1dda8377294
AlexBor78 <you@example.com>
2026-06-29 12:32:22 +0300

    added anton to wg vpn

--- nix/hosts/anton/network.nix
+++ nix/hosts/anton/network.nix
@@ -3,20 +3,20 @@
 
 	# vpn for server
 	networking.wireguard.enable = true;
-#  networking.wireguard.interfaces.wg0 = {
-#    ips = [ "10.0.0.7/24" ];
-#    privateKeyFile = "/run/secrets/wg-private";
-#    
-#    peers = [
-#      {
-#        publicKey = "wJ0ynClXDC8OVsGpy/cgCpMmJAH8QDHrUK0PNoMJSn0=";
-#        endpoint = "188.68.223.213:51820";
-#        
-#        allowedIPs = [ "10.0.0.0/24" ];
-#        persistentKeepalive = 25;
-#      }
-#    ];
-#  };
+  networking.wireguard.interfaces.wg0 = {
+    ips = [ "10.0.0.2/24" ];
+    privateKeyFile = "/var/lib/wireguard/privatekey";
+    
+    peers = [
+      {
+        publicKey = "wJ0ynClXDC8OVsGpy/cgCpMmJAH8QDHrUK0PNoMJSn0=";
+        endpoint = "188.68.223.213:51820";
+        
+        allowedIPs = [ "10.0.0.0/24" ];
+        persistentKeepalive = 25;
+      }
+    ];
+  };
 #
 #  # firewall
 #  networking.firewall = {
